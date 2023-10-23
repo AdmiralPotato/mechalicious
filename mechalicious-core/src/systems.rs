7d87bc27b05bed169ea8320e34f29495f177118a
Admiral Potato <admiral@nuclearpixel.com>
2023-10-23 17:51:04 -0600

    Extract GameWorld systems into module
    Co-authored-by: Solra Bizna <solra@bizna.name>

---
+++ mechalicious-core/src/systems.rs
@@ -0,0 +1,54 @@
+use super::*;
+
+impl GameWorld {
+    pub fn tick(&mut self, inputs: &[(EntityId, &ShipControls)]) {
+        self.prev_ecs_world = self.ecs_world.clone();
+        self.ecs_world = self.ecs_world.buffered_tick(|world| {
+            // this is where our Systems go
+            for (entity_id, player_controls) in inputs {
+                if let Some(mut controls) = ecs_get!(world, *entity_id, mut ShipControls) {
+                    (*controls).clone_from(player_controls);
+                } else {
+                    eprintln!("WARNING: missing player {entity_id:?}");
+                }
+            }
+            // Ship Controls System
+            for (_entity_id, placement, controls, physics) in
+                ecs_iter!(world, mut Placement, cur ShipControls, mut Physics)
+            {
+                physics.apply_force(controls.movement * 0.005);
+                let facing_angle = placement.angle;
+                let aim_angle = controls.aim.y.atan2(controls.aim.x);
+                let diff = angle_subtract(facing_angle, aim_angle);
+                physics.apply_torque(diff * 0.005);
+            }
+            // Physics System
+            let world_physics = ecs_singleton!(world, cur WorldPhysics);
+            for (_entity_id, position, physics) in ecs_iter!(world, mut Placement, mut Physics) {
+                let drag_amount =
+                    physics.velocity.magnitude_squared() * -world_physics.air_thickness;
+                if drag_amount != 0.0 {
+                    physics.apply_force(physics.velocity.normalize() * drag_amount);
+                }
+                // TODO: angular fiziks
+                /*
+                let angular_drag_amount =
+                    physics.angular_velocity.abs() * physics.angular_velocity * -0.1;
+                if angular_drag_amount != 0.0 {
+                    physics.apply_torque(angular_drag_amount);
+                }
+                */
+                let linear_acceleration = physics.force / physics.mass;
+                let angular_acceleration = physics.torque / physics.moment;
+                position.position += physics.velocity + linear_acceleration * 0.5;
+                position.angle += physics.angular_velocity + angular_acceleration * 0.5;
+                position.angle = position.angle % TAU;
+                physics.velocity += linear_acceleration;
+                physics.angular_velocity += angular_acceleration;
+                // zero out the forces for next tick
+                physics.force = vector![0.0, 0.0];
+                physics.torque = 0.0;
+            }
+        });
+    }
+}
